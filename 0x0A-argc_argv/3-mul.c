a67c45c51aac9923897d27428a62ad64c0d8a39d
Yahaya Abbas Yakubu <crescentsalam16@gmail.com>
2023-10-02 03:12:02 -0700

    a program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - change  string to integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int a, d, n, len, f, digit;
+
+	a = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (a < len && f == 0)
+	{
+		if (s[a] == '-')
+			++d;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			digit = s[a] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
+				break;
+			f = 0;
+		}
+		a++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplications of two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, numA, numB;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	numA = _atoi(argv[1]);
+	numB = _atoi(argv[2]);
+	result = numA * numB;
+
+	printf("%d\n", result);
+
+	return (0);
+}
